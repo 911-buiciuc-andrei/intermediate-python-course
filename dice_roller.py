31db095e8dc7762bdc967e2068a685b8d10f6473
911-buiciuc-andrei <72063066+911-buiciuc-andrei@users.noreply.github.com>
2020-10-26 17:20:05 +0200

    update

--- dice_roller.py
+++ dice_roller.py
@@ -2,13 +2,14 @@ import random
 
 def main():
 
-  dice_rolls = 3
+  dice_rolls = int(input("The number of dice throws: "))
+  dice_size = int(input("The number of sides for a dice: "))
   dice_sum = 0
   while dice_rolls:
-    roll = random.randint(1, 6)
+    roll = random.randint(1, dice_size)
     if roll == 1:
         print(f'You rolled a {roll}! Critical Fail!')
-    elif roll == 6:
+    elif roll == dice_size:
         print(f'You rolled a {roll}! Critical Success!')
     else:
         print(f'You rolled a {roll}')
@@ -16,6 +17,6 @@ def main():
     dice_rolls -= 1
   print(f'You rolled a total of: {dice_sum}')
 
-  
+
 if __name__== "__main__":
   main()
